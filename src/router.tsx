612ea8522832ad53bf1f506321e718834cf96fbf
KriXPello <krixpello@gmail.com>
2026-02-15 20:11:46 +0300

    fix router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, replace } from 'react-router';
+import { createHashRouter, replace } from 'react-router';
 import { AuthPage } from '~/pages/auth';
 import { ProductsPage } from '~/pages/products';
 import { authMiddleware } from '~/shared/auth/middleware';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/auth',
     Component: AuthPage,
@@ -16,6 +16,4 @@ export const router = createBrowserRouter([
       { path: 'products', Component: ProductsPage },
     ],
   },
-], {
-  basename: import.meta.env.BASE_URL,
-});
+]);
